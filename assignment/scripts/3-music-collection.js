301f566eeebfb0ce1b855ad601cf8426d0ed2b4c
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-10-22 11:35:03 -0500

    Stretch Goal

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -90,6 +90,55 @@ console.table(findByArtist(myCollection, "Savant"))
 console.table(findByArtist(myCollection, "KITCALIBER"))
 console.table(findByArtist(myCollection, "me"))
 
+// So I looked at the tests for this, and they're using two different structures for this
+// As such I went ahead and made two search criteria types, and let my code be compatible with both
+/**
+ * @typedef {Object} SearchCriteriaA
+ * @property {string} [artist]
+ * @property {number} [year]
+ */
+/**
+ * @typedef {Object} SearchCriteriaB
+ * @property {string} [artist]
+ * @property {number} [yearPublished]
+ */
+/**
+ * @typedef {SearchCriteriaA | SearchCriteriaB} SearchCriteria
+ */
+
+/**
+ * Searches albums
+ * @param {Album[]} collection
+ * @param {SearchCriteria?} criteria
+ * @returns {Album[]}
+ */
+function search(collection, criteria) {
+  if (!criteria) {
+    return collection
+  }
+  const { artist } = criteria
+  const year = criteria.year || criteria.yearPublished
+  if (!artist || !year) {
+    return collection
+  }
+  const results = []
+  for (const album of collection) {
+    if (
+      artist &&
+      year &&
+      album.artist === artist &&
+      album.yearPublished === year
+    ) {
+      results.push(album)
+    }
+  }
+  // if (results.length === 0) {
+  //   return collection
+  // }
+  return results
+}
+console.table(search(myCollection, { artist: "Savant", year: 2012 }))
+
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
 // (It's used for automated testing.)
